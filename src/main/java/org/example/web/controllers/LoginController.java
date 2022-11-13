6508174d4b576054ca01bd74cd4a60cc93a150ce
Alexandr Krasnikov <tankist_s91@mail.ru>
2022-11-13 10:44:10 +0400

    add exceptions

--- src/main/java/org/example/web/controllers/LoginController.java
+++ src/main/java/org/example/web/controllers/LoginController.java
@@ -1,11 +1,13 @@
 package org.example.web.controllers;
 
 import org.apache.log4j.Logger;
+import org.example.app.exceptions.BookShelfLoginException;
 import org.example.app.services.LoginService;
 import org.example.web.dto.LoginFrom;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,14 +33,19 @@ public class LoginController {
 
     //Придумать как создать уведомление для пользователя о неправильном вводе пароля
     @PostMapping("/auth")
-    public String authenticate(LoginFrom loginFrom) {
+    public String authenticate(LoginFrom loginFrom) throws BookShelfLoginException {
         if(loginService.authenticate(loginFrom)) {
             logger.info("login OK redirect to book shelf");
             return "redirect:/books/shelf";
         } else {
             logger.info("login FAIL redirect back to login");
-            return "redirect:/login";
+            throw new BookShelfLoginException("invalid username or password");
         }
-
     }
+
+    @ExceptionHandler(BookShelfLoginException.class)
+    public String handleError() {
+        return "errors/404";
+    }
+
 }
